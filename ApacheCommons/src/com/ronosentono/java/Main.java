ff2d5d3e00a9af00c5807e6679c5fd1db49ecf7b
Mohammad Ronosentono <mohammad.ronosentono@gmail.com>
2022-07-24 18:54:37 -0400

    Add Common Java Libraries

---
+++ ApacheCommons/src/com/ronosentono/java/Main.java
@@ -0,0 +1,23 @@
+package com.ronosentono.java;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        File sourceFile = new File("files/loremipsum.txt");
+        File targetFile = new File("files/target.txt");
+
+        try {
+            FileUtils.copyFile(sourceFile, targetFile);
+            System.out.println("File copied!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
